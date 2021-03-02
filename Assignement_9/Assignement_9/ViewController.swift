d8b2459a98dfd6c700ad52148ba85198259c6423
Titanscull <ispetrovskyi@gmail.com>
2021-03-02 22:38:13 +0200

    Blank project created

---
+++ Assignement_9/Assignement_9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignement_9
+//
+//  Created by Ievgen Petrovskiy on 02.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
