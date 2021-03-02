4a3eead8d6ba2950000b079219a631de072b8c7d
Titanscull <ispetrovskyi@gmail.com>
2021-03-02 23:04:18 +0200

    Table View & Cell connected to controller

--- Assignement_9/Assignement_9/ViewController.swift
+++ Assignement_9/Assignement_9/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainTableView: UITableView!
+    
+    @IBOutlet weak var cellView: UITableViewCell!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
     }
 
 
